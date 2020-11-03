eb8052d788b81a8e03af43fca817576bfde45778
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 14:32:05 +0100

    Enable test_49_is_not_40_5_4_but_rather_50_10_10_1

--- tests/tests_zad02.py
+++ tests/tests_zad02.py
@@ -34,7 +34,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(self.temp.roman(48), "XLVIII")
 
-    @unittest.skip("Solution not added")
     def test_49_is_not_40_5_4_but_rather_50_10_10_1(self):
         self.assertEqual(self.temp.roman(49), "XLIX")
 
