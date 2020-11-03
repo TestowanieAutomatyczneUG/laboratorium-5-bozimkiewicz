c2e3d4899f49ed4c15a6150e035dbdf7c1caa39a
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 14:22:47 +0100

    Enable test_4_being_5_1_is_iv

--- tests/tests_zad02.py
+++ tests/tests_zad02.py
@@ -16,7 +16,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_3_is_three_i_s(self):
         self.assertEqual(self.temp.roman(3), "III")
 
-    @unittest.skip("Solution not added")
     def test_4_being_5_1_is_iv(self):
         self.assertEqual(self.temp.roman(4), "IV")
 
