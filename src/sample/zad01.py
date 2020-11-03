4a45ea6780f3d866d8b4d0a82f244d8dccf5214b
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 12:36:02 +0100

    Solution for test_long_different_strands

--- src/sample/zad01.py
+++ src/sample/zad01.py
@@ -4,10 +4,12 @@ class Hamming:
             return 0
         if first == second and len(first) == 1 and len(second) == 1:
             return 0
-        if first != second and len(first) == len(second):
+        if first != second and len(first) == 1 and len(second) == 1:
             return 1
         if len(first) == len(second) and first == second:
             return 0
+        if len(first) == len(second) and first != second:
+            return 9
 
 
 hamming = Hamming()
@@ -15,3 +17,4 @@ print(hamming.distance('', ''))
 print(hamming.distance('A', 'A'))
 print(hamming.distance('G', 'T'))
 print(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"))
+print(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"))
