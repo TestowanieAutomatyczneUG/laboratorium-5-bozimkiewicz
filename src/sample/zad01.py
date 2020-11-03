9b5c7abc645a68243a6421b0f512f54a294a95ef
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 13:07:34 +0100

    Refactor code

--- src/sample/zad01.py
+++ src/sample/zad01.py
@@ -1,27 +1,13 @@
 class Hamming:
     def distance(self, first, second):
-        if first == '' and second == '':
-            return 0
-        if first == second and len(first) == 1 and len(second) == 1:
-            return 0
-        if first != second and len(first) == 1 and len(second) == 1:
-            return 1
-        if len(first) == len(second) and first == second:
-            return 0
-        if len(first) == len(second) and first != second:
-            counter = 0
-            for i in range(0, len(first)):
-                if first[i] != second[i]:
-                    counter += 1
-            return counter
-        if len(first) > len(second):
-            raise ValueError('First can\'t be longer than second')
-        if len(first) < len(second):
-            raise ValueError('Second can\'t be longer than first')
-        if len(first) == 0 and len(second) > 0:
-            raise ValueError('First can\'t be empty')
-        if len(second) == 0 and len(first) > 0:
-            raise ValueError('Second can\'t be empty')
+        if len(first) != len(second):
+            raise ValueError('Values cannot be of different length')
+        counter = 0
+        for i in range(0, len(first)):
+            if first[i] != second[i]:
+                counter += 1
+        return counter
+
 
 
 hamming = Hamming()
