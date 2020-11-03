9ce3a08eec74a8fd310eca8410430810e9af67d3
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 12:19:20 +0100

    Enable test_single_letter_identical_strands

--- tests/test_zad01.py
+++ tests/test_zad01.py
@@ -15,7 +15,6 @@ class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
         self.assertEqual(self.temp.distance("", ""), 0)
 
-    @unittest.skip('Solution not added')
     def test_single_letter_identical_strands(self):
         self.assertEqual(self.temp.distance("A", "A"), 0)
 
