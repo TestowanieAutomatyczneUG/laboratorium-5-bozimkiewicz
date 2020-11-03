53c263abea4b4a2739094f8565e496a767f36552
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 14:21:41 +0100

    Enable test_3_is_three_i_s

--- tests/tests_zad02.py
+++ tests/tests_zad02.py
@@ -13,7 +13,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_2_is_two_i_s(self):
         self.assertEqual(self.temp.roman(2), "II")
 
-    @unittest.skip("Solution not added")
     def test_3_is_three_i_s(self):
         self.assertEqual(self.temp.roman(3), "III")
 
