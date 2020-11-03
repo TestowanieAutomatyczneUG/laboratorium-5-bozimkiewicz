c60da90c4477c9ab47cfae02c07aee7cdfebbcdf
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 12:23:26 +0100

    Solution for test_single_letter_identical_strands

--- src/sample/zad01.py
+++ src/sample/zad01.py
@@ -2,7 +2,10 @@ class Hamming:
     def distance(self, first, second):
         if first == '' and second == '':
             return 0
+        if first == second:
+            return 0
 
 
 hamming = Hamming()
 print(hamming.distance('', ''))
+print(hamming.distance('A', 'A'))
