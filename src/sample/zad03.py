6a2dc4167ab492c5172caa9365c53e066897100b
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 20:49:39 +0100

    Solution for test_print_one_line

--- src/sample/zad03.py
+++ src/sample/zad03.py
@@ -14,7 +14,7 @@ class Song:
                      'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.']
 
     def one_line(self, num):
-        return ''
+        return self.song[num - 1]
 
     def section(self, start, end):
         return []
