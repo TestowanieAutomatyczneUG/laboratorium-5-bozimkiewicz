74e5868a1090d29fd1820d95ff57dec03c0902d5
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 12:27:14 +0100

    Solution for test_single_letter_different_strands

--- src/sample/zad01.py
+++ src/sample/zad01.py
@@ -4,8 +4,11 @@ class Hamming:
             return 0
         if first == second:
             return 0
+        if first != second and len(first) == len(second):
+            return 1
 
 
 hamming = Hamming()
 print(hamming.distance('', ''))
 print(hamming.distance('A', 'A'))
+print(hamming.distance('G', 'T'))
