06975620ab31f6058afcd7cf77d754e68b5075ed
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 14:26:58 +0100

    Enable test_9_being_10_1_is_ix

--- tests/tests_zad02.py
+++ tests/tests_zad02.py
@@ -25,7 +25,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_6_being_5_1_is_vi(self):
         self.assertEqual(self.temp.roman(6), "VI")
 
-    @unittest.skip("Solution not added")
     def test_9_being_10_1_is_ix(self):
         self.assertEqual(self.temp.roman(9), "IX")
 
