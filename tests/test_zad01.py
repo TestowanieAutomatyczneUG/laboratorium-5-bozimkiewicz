084ab48a9402c0297472bd85b82373eb6544fe2a
bozimkiewicz <bartek.ozim@wp.pl>
2020-11-03 12:33:56 +0100

    Enable test_long_different_strands

--- tests/test_zad01.py
+++ tests/test_zad01.py
@@ -24,7 +24,6 @@ class HammingTest(unittest.TestCase):
     def test_long_identical_strands(self):
         self.assertEqual(self.temp.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
-    @unittest.skip('Solution not added')
     def test_long_different_strands(self):
         self.assertEqual(self.temp.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
